771c75c0cf6bb14fd6c6b5e2c4248cd4e98ac831
Georg Schwarz <schwarz@group.riehle.org>
2026-02-20 16:58:16 +0100

    refactor(site): centralize site title

---
+++ src/data/site.ts
@@ -0,0 +1,7 @@
+export const SITE_TITLE = "Idol or Bust!";
+export const SITE_DESCRIPTION =
+  "Simple website software for research projects that want to understand their user needs";
+export const SITE_BASE_URL = "https://riehlegroup.github.io";
+export const BLOG_TITLE = "Idol or Bust Blog";
+export const BLOG_DESCRIPTION =
+  "Latest updates and insights from the Idol or Bust research project.";
