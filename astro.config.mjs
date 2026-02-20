5fb216d2cb39a7838766bce087fa5333f1e7b203
Georg Schwarz <schwarz@group.riehle.org>
2026-02-20 15:17:42 +0100

    fix(deploy): set pages base and og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,24 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+const repo = "idolbust";
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
-  // base: '/idolbust', //TODO: Needed for GitHub Pages deployment only, not locally.
-  integrations: [
-    tailwind(),
-    mdx(),
-    sitemap(),
-  ],
+  site: "https://riehlegroup.github.io",
+  base: process.env.NODE_ENV === "production" ? `/${repo}/` : "/",
+  integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
+      theme: "github-dark",
       wrap: true,
     },
   },
   vite: {
     resolve: {
       alias: {
-        '@': '/src',
+        "@": "/src",
       },
     },
   },
