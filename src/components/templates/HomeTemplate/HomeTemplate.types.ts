2af178b322ca076307567f569dc9a468d0bfab32
Georg Schwarz <schwarz@group.riehle.org>
2026-03-04 11:00:28 +0100

    fix(templates): inline brand color scale type

--- src/components/templates/HomeTemplate/HomeTemplate.types.ts
+++ src/components/templates/HomeTemplate/HomeTemplate.types.ts
@@ -1,5 +1,18 @@
 import type { FeatureItem } from "@/components/molecules/FeatureGrid/FeatureGrid.types";
-import type { BrandColorScale } from "@/data-models/brand";
+
+interface BrandColorScale {
+  "50": string;
+  "100": string;
+  "200": string;
+  "300": string;
+  "400": string;
+  "500": string;
+  "600": string;
+  "700": string;
+  "800": string;
+  "900": string;
+  "950": string;
+}
 
 export interface HomeTemplateBrand {
   identity: {
