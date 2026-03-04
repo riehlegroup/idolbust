46b1df0345fe1782e357327718125badb65144ea
Georg Schwarz <schwarz@group.riehle.org>
2026-03-04 10:09:24 +0100

    chore(lint): enforce component boundaries

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import astro from "eslint-plugin-astro";
+import boundaries from "eslint-plugin-boundaries";
 import tsParser from "@typescript-eslint/parser";
 
 export default [
@@ -7,6 +8,9 @@ export default [
   },
   ...astro.configs["flat/recommended"],
   {
+    plugins: {
+      boundaries,
+    },
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       parser: tsParser,
@@ -14,6 +18,83 @@ export default [
         ecmaVersion: "latest",
         sourceType: "module",
       },
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json",
+        },
+      },
+      "boundaries/elements": [
+        {
+          type: "component-utils",
+          pattern: "components/utils/*.{ts,tsx,js,jsx}",
+          mode: "file",
+          capture: ["elementName"],
+        },
+        {
+          type: "atoms",
+          pattern: "components/atoms/*",
+          mode: "folder",
+          capture: ["elementName"],
+        },
+        {
+          type: "molecules",
+          pattern: "components/molecules/*",
+          mode: "folder",
+          capture: ["elementName"],
+        },
+        {
+          type: "organisms",
+          pattern: "components/organisms/*",
+          mode: "folder",
+          capture: ["elementName"],
+        },
+        {
+          type: "templates",
+          pattern: "components/templates/*",
+          mode: "folder",
+          capture: ["elementName"],
+        },
+      ],
+    },
+    rules: {
+      ...boundaries.configs.recommended.rules,
+      "boundaries/no-unknown": 2,
+      "boundaries/element-types": [
+        2,
+        {
+          default: "disallow",
+          rules: [
+            {
+              from: ["atoms"],
+              allow: ["atoms", "component-utils"],
+            },
+            {
+              from: ["molecules"],
+              allow: ["atoms", "molecules", "component-utils"],
+            },
+            {
+              from: ["organisms"],
+              allow: ["atoms", "molecules", "organisms", "component-utils"],
+            },
+            {
+              from: ["templates"],
+              allow: [
+                "atoms",
+                "molecules",
+                "organisms",
+                "templates",
+                "component-utils",
+              ],
+            },
+            {
+              from: ["component-utils"],
+              allow: ["component-utils"],
+            },
+          ],
+        },
+      ],
     },
   },
 ];
